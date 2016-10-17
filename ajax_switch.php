7986218798dac2ac08996bcd487b0379a47e2edd
kruftindustries <kd0ohr@gmail.com>
2016-10-16 20:57:29 -0500

    Update ajax_switch.php
    fixed some variable names

--- ajax_switch.php
+++ ajax_switch.php
@@ -1,11 +1,11 @@
 <?php
 
 function float_rand($Min, $Max, $round=0){
-    //validate input
-    if ($min&gt;$Max) { $min=$Max; $max=$Min; }
-        else { $min=$Min; $max=$Max; }
-    $randomfloat = $min + mt_rand() / mt_getrandmax() * ($max - $min);
-    if($round&gt;0)
+    #validate input
+    if ($Min > $Max) { $Min = $Max; $Max = $Min; }
+        else { $Min = $Min; $Max = $Max; }
+    $randomfloat = $Min + mt_rand() / mt_getrandmax() * ($Max - $Min);
+    if($round > 0)
         $randomfloat = round($randomfloat,$round);
  
     return $randomfloat;
